44df32aad61fcb62d3d490447d06c5c496b1e0c7
Shangqi Lai <shangqi.lai@monash.edu>
2020-02-27 11:48:09 +0800

    Init commit

---
+++ App/App.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "sgx_urts.h"
+#include "Enclave_u.h"
+
+#define ENCLAVE_FILE "Enclave.signed.so"
+
+using namespace std;
+
+void ocall_print_string(const char *str) {
+    printf("%s\n", str);
+}
+
+int main() {
+    /* Setup enclave */
+    sgx_enclave_id_t eid;
+    sgx_status_t ret;
+    sgx_launch_token_t token = { 0 };
+    int token_updated = 0;
+
+    ret = sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &token_updated, &eid, NULL);
+    if (ret != SGX_SUCCESS)
+    {
+        cout <<"sgx_create_enclave failed: 0x" << std::hex << ret << endl;
+        return 1;
+    }
+
+    uint8_t *test_buffer = (uint8_t*) malloc(1000);
+
+    ecall_test(eid, test_buffer, 100);
+}
---
+++ Enclave/EnclaveUtil.h
@@ -0,0 +1,14 @@
+#ifndef MEASUREMENT_ENCLAVEUTIL_H
+#define MEASUREMENT_ENCLAVEUTIL_H
+
+#include <string>
+#include <unordered_map>
+
+#include "sgx_trts.h"
+#include "Enclave_t.h"
+
+using namespace std;
+
+void printf( const char *fmt, ...);
+
+#endif //MEASUREMENT_ENCLAVEUTIL_H
